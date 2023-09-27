0339dbf7f2c0523788c9cfb7a58c6488037b8887
Yaroslava Mala <ymala@kse.org.ua>
2023-09-27 21:30:28 +0300

    added second task

--- scripts/script.js
+++ scripts/script.js
@@ -15,3 +15,60 @@ document.getElementById('inputText').addEventListener('input', () => {
     clearTimeout(window.inputTimeout);
     window.inputTimeout = setTimeout(updateOutput, 1000);
 });
+
+// second task
+
+const apiKey = 'MeKagcUG1UCC3SGWuD2wlwGLDKj1iOCOjhCD59gg'; 
+
+async function fetchAPOD(date) {
+    try {
+        const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`);
+        if (!response.ok) {
+            throw new Error('Error fetching data from NASA API.');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+function updateData(data) {
+    const image = document.getElementById('imageNASA');
+    const description = document.getElementById('description');
+    image.src = data.url;
+    description.textContent = data.explanation;
+}
+
+function buttonClick(event) {
+    const date = event.target.dataset.date;
+    fetchAPOD(date)
+        .then((data) => {
+            updateData(data);
+            const containerAPOD = document.querySelector('.containerAPOD');
+            containerAPOD.style.display = 'block';
+            const descriptionError = document.getElementById('descriptionError');
+            descriptionError.textContent = '';
+        })
+        .catch((error) => {
+            const containerAPOD = document.querySelector('.containerAPOD');
+            containerAPOD.style.display = 'none';
+            const descriptionError = document.getElementById('descriptionError');
+            descriptionError.textContent = 'An error occurred while fetching data: ' + error.message;
+        })
+        .finally(() => {
+            alert('Request completed.');
+        });
+}
+
+const buttonsContainer = document.getElementById('buttonsContainer');
+for (let day = 1; day <= 30; day++) {
+    const button = document.createElement('button');
+    button.className = 'buttons';
+    button.textContent = `September ${day}`;
+    button.dataset.date = `2023-09-${day}`;
+    button.addEventListener('click', buttonClick);
+    buttonsContainer.appendChild(button);
+}
+
+// third task
